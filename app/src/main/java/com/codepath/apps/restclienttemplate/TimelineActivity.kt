6b9a5a0d4be96b451d46f56a6cf5e01c624bc8c6
Benaldofire <Odaibenedict@gmail.com>
2022-03-11 22:59:39 -0600

    Completed Requirements. Implemented pull to refresh and populated page with the tweets.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -3,19 +3,57 @@ package com.codepath.apps.restclienttemplate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import okhttp3.internal.notify
+import org.json.JSONException
 
 class TimelineActivity : AppCompatActivity() {
 
     lateinit var client: TwitterClient
+
+    lateinit var rvTweets: RecyclerView
+
+    lateinit var adapter: TweetsAdapter
+
+    val tweets = ArrayList<Tweet>()
+
+    lateinit var swipeContainer: SwipeRefreshLayout
+
     val TAG = "TimelineActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
 
         client = TwitterApplication.getRestClient(this)
-        //populateHomeTimeline()
+
+        swipeContainer = findViewById(R.id.swipeContainer)
+
+        swipeContainer.setOnRefreshListener {
+            Log.i(TAG,"Refreshing Timeline")
+            populateHomeTimeline()
+        }
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light
+        );
+
+
+        rvTweets = findViewById(R.id.rvTweets)
+        adapter = TweetsAdapter(tweets)
+
+        rvTweets.layoutManager = LinearLayoutManager(this)
+        rvTweets.adapter = adapter
+
+        populateHomeTimeline()
+
     }
 
     fun populateHomeTimeline() {
@@ -30,8 +68,25 @@ class TimelineActivity : AppCompatActivity() {
                 Log.i(TAG, "onFailure $response")
             }
 
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
-                Log.i(TAG, "Successful: Retrieving Timeline $json")
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.i(TAG, "Successful")
+
+                val jsonArray = json.jsonArray
+
+                try {
+                    //clear current tweets
+                    adapter.clear()
+                    //fromJsonArray method parses a whole json array into a list of tweets
+                    val listOfNewTweets = Tweet.fromJsonArray(jsonArray)
+                    tweets.addAll(listOfNewTweets)
+                    adapter.notifyDataSetChanged()
+
+                    // Now we call setRefreshing(false) to signal refresh has finished
+                    swipeContainer.setRefreshing(false)
+
+                } catch (e: JSONException) {
+                    Log.e(TAG, "JSON Exception $e")
+                }
             }
 
         })
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
@@ -0,0 +1,64 @@
+package com.codepath.apps.restclienttemplate
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.codepath.apps.restclienttemplate.models.Tweet
+import org.w3c.dom.Text
+
+class TweetsAdapter (val tweets: ArrayList<Tweet>) : RecyclerView.Adapter<TweetsAdapter.ViewHolder>() {
+
+    //responsible for inflating the layout we want to use for each item we need to display
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetsAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        //inflate our item layout
+        val view = inflater.inflate(R.layout.item_tweet,parent,false)
+
+        //return viewholder with the the view we just inflated
+        return ViewHolder(view)
+    }
+
+    //populating data into the item through holder
+    override fun onBindViewHolder(holder: TweetsAdapter.ViewHolder, position: Int) {
+        //Get the data model based on position
+        val tweet: Tweet = tweets.get(position)
+
+        //Set item views based on views and data model
+
+        holder.tvUsername.text = tweet.user?.name
+        holder.tvTweetBody.text = tweet.body
+
+        //use glide for images. requires a context,
+        //so use the itemView(inflated view from onCreate) of the viewHolder class
+        Glide.with(holder.itemView).load(tweet.user?.publicImgUrl).into(holder.ivProfileImage)
+    }
+
+    //how many views will be in our recyclerView
+    override fun getItemCount(): Int {
+        return tweets.size
+    }
+
+    // Clean all elements of the recycler
+    fun clear() {
+        tweets.clear()
+        notifyDataSetChanged()
+    }
+
+    // Add a list of items -- change to type used
+    fun addAll(tweetList: List<Tweet>) {
+        tweets.addAll(tweetList)
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivProfileImage = itemView.findViewById<ImageView>(R.id.ivProfileImage)
+        val tvUsername = itemView.findViewById<TextView>(R.id.tvUsername)
+        val tvTweetBody = itemView.findViewById<TextView>(R.id.tvTweetBody)
+    }
+}
